8080ee3269aaed49d1fe80deca81843832f958ea
maxshmax666 <maxshirakov@gmail.com>
2026-04-12 22:30:40 +0500

    feat: implement pixel-perfect mobile and tablet pizza home UI

--- src/app/layout/RootLayout.tsx
+++ src/app/layout/RootLayout.tsx
@@ -1,15 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 
 export function RootLayout() {
+  const location = useLocation();
+  const isShowcaseHome = location.pathname === "/";
+
   return (
     <div className="min-h-full flex flex-col">
-      <Header />
+      {!isShowcaseHome ? <Header /> : null}
       <main className="flex-1">
         <Outlet />
       </main>
-      <Footer />
+      {!isShowcaseHome ? <Footer /> : null}
     </div>
   );
 }
